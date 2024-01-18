81d2df31bc384f5c3b48dc7883792a2d9e56023f
Christian Gleerup <cromozon@swn.nu>
2024-01-18 01:03:29 +0100

    Added code to count number of options for input fields and map back to influenced equations

--- YTACM-BRAIN/app/solve.py
+++ YTACM-BRAIN/app/solve.py
@@ -7,9 +7,22 @@ class FieldTypes:
         self.cols = 0
         self.fields = []
     
+    # fieldTypes, 0=bg, 1=input, 2= symbol or operator
     def getField(self, r,c):
         return self.fields[c+r*self.cols]
-
+    
+    def isBg(self, r,c):
+        return self.getField(r,c)==0
+    
+    def isInput(self, r,c):
+        return self.getField(r,c)==1
+    
+    def isSymbol(self, r,c):
+        return self.getField(r,c)==2
+    
+    def isBREdge(self, r,c):
+        return r == (self.rows-1) or c == (self.cols-1)
+    
     # Create field types expecting filePointer to be matching octave output
     def BuildFieldTypes(fp):
         Y = FieldTypes()
@@ -23,6 +36,7 @@ class FieldTypes:
 class SymbolsWithPos:
     def __init__(self):
         self.symbols = {}
+    
     def BuildSymbolsWithPos(fp):
         Y = SymbolsWithPos()
         nSymbols = int(fp.readline().strip())
@@ -31,6 +45,11 @@ class SymbolsWithPos:
             Y.symbols[pos] = sym
         return Y
     
+    # Gets symbol at row, column,
+    def getSymbol(self, r, c):
+        return self.symbols[str.format("{},{}",r+1,c+1)] # Adding one to match Octave indices
+    
+
 class GameState:
     def __init__(self, filename: str):
         with open(filename) as fp:
@@ -41,11 +60,80 @@ class GameState:
                     self.symbolWithPos = SymbolsWithPos.BuildSymbolsWithPos(fp)
                 if line.startswith("variables_with_pos="):
                     self.variablesWithPos = SymbolsWithPos.BuildSymbolsWithPos(fp)
+        self.CreateEquations()
     
+    def _rowAndColIter(self):
+        for r in range(self.fieldTypes.rows) :
+            for c in range(self.fieldTypes.cols) :
+                yield r,c
+        
+        for c in range(self.fieldTypes.cols) :
+            for r in range(self.fieldTypes.rows) :
+                yield r,c
+
+    def CreateEquations(self):
+        T = self.fieldTypes
+        S = self.symbolWithPos
+        
+        self.equations = []
+        self.inputOptions = set()
+        
+        currentEquation = ""
+        currentEquationVariables = set()
+        i = 0
+        for r,c in self._rowAndColIter() :
+            if T.isSymbol(r,c):
+                i+=1
+                currentEquation =  currentEquation+ S.getSymbol(r,c)
+            elif T.isInput(r,c):
+                i+=1
+                currentEquation = currentEquation + '_'
+                currentEquationVariables.add((r,c))
+                self.inputOptions.add((r,c))
+            elif i>1:
+                self.equations.append((currentEquation, currentEquationVariables))
+                currentEquation=""
+                currentEquationVariables=set()
+                i=0
+            elif i==1:
+                currentEquation=""
+                currentEquationVariables=set()
+                i=0
+            # Special case to make sure no wrap around and no missing equation
+            if (T.isBREdge(r,c) and i>1):
+                self.equations.append((currentEquation, currentEquationVariables))
+                currentEquation=""
+                currentEquationVariables=set()
+                i=0
+            if (T.isBREdge(r,c) and i==1):
+                currentEquation=""
+                currentEquationVariables=set()
+                i=0
+
+        # count minimum number of related input for a specific input coordinate
+        #for opt in inputOptions:
+
+
     def PrintState(self):
         print(self.fieldTypes.fields)
         print(self.variablesWithPos.symbols)
         print(self.symbolWithPos.symbols)
+
                     
 gameState = GameState('/shared/cross-math-scan-result.txt')
-gameState.PrintState()
+#gameState.PrintEquations()
+print("equations=")
+print(gameState.equations)
+
+print("inputOptions=")
+print(gameState.inputOptions)
+
+print("equations pr input=")
+for opt in gameState.inputOptions:
+    eqFound = []
+    for eqWithOpts in gameState.equations:
+        if (opt in eqWithOpts[1]):
+            eqFound.append(eqWithOpts[0])
+    print(opt)
+    print(eqFound)
+    print(min(map(lambda x:x.count('_'), eqFound)))
