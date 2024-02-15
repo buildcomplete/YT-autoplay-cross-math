802a08aaf1f4c3963d79948378d88d45e8db120e
Christian Gleerup <buildcomplete@proton.me>
2024-02-15 11:17:01 +0300

    Corrected node visisted count Consolidated two figures into one Added detailed processing steps diagram

--- YTACM-EYE/app/optimization_analysis.m
+++ YTACM-EYE/app/optimization_analysis.m
@@ -1,20 +1,20 @@
 # Optimization analysis
 
 #nCombinations
-# worst, bruteforce, best
-expert=[5789328 1708 338]./2
-difficult=[3495390 4672 216]./2
-medium=[7850 366 80]./2
-easy=[88 88 40]./2
+# most unknowns first, unsorted, fewest unknowns first
+expert=[2894674 864 179]
+difficult=[14698240	468	237]
+medium=[3933	468	48]
+easy=[49	44	25]
 
 subplot(1,2,1)
 plot([easy;medium;difficult;expert]');
 title('number of visits based on sorting algorithm')
 legend(['easy'; 'medium'; 'difficult'; 'expert']);
-set(gca, 'XTick', 1:3, 'XTickLabel', {'worst'; 'no sorting'; 'best'});
+set(gca, 'XTick', 1:3, 'XTickLabel', {'most unknowns first'; 'no sorting'; 'fewest unknowns first'});
 
 subplot(1,2,2)
 semilogy([easy;medium;difficult;expert]');
 title('number of visits based on sorting algorithm')
 legend(['easy'; 'medium'; 'difficult'; 'expert']);
-set(gca, 'XTick', 1:3, 'XTickLabel', {'worst'; 'no sorting'; 'best'});
+set(gca, 'XTick', 1:3, 'XTickLabel', {'most unknowns first'; 'no sorting'; 'fewest unknowns first'});
