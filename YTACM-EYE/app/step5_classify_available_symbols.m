3929f00465595f18452cad13afff88856a12722f
Christian Gleerup <buildcomplete@proton.me>
2024-12-08 09:00:17 +0100

    Updated to dynamic border size in classify variables

--- YTACM-EYE/app/step5_classify_available_symbols.m
+++ YTACM-EYE/app/step5_classify_available_symbols.m
@@ -5,7 +5,7 @@
 % rectSize % rectSize
 % varIndexEnergy % the energy at a specific index, if zero the is no variable at the entry
 
-wh = (rectSize / 2) - 11;
+wh = (rectSize / 2) - 1;
 variables_with_pos = "";
 
 rSymLib = symbolLib([1:10 18]);
@@ -19,7 +19,11 @@ for idR = 1:length(varRowIdx)
       c2 = varColIdx(idC) + wh;
 
       I = variables(r1:r2, c1:c2);
-      B = (1 - (I < I(1)-10)) .* 255; % Remove shadow noise by binarization, assuming dark letter on brighter bg
+      B = (1 - (I < max(I(:))-10)) .* 255; % Remove shadow noise by binarization, assuming dark letter on brighter bg
+      zr= sum(B,2)==0;
+      zc= sum(B,1)==0;
+      B(zr,:)=255;
+      B(:,zc)=255;
 
       sIds = sub_find_symbols(B, rSymLib);
       variables_with_pos = [variables_with_pos; sprintf("%d,%d:%s", idR, idC, rSymNames(sIds))];
